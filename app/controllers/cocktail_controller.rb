e2074e84be0ca3a65858a37c51d1fe5c73def983
Daniel Tuppen <daniel.tuppen1@gmail.com>
2021-01-30 13:12:39 +0000

    completed cocktails controller

--- app/controllers/cocktail_controller.rb
+++ app/controllers/cocktail_controller.rb
@@ -1,2 +1,35 @@
 class CocktailController < ApplicationController
+  before_action :set_cocktail, only: [:show, :edit]
+
+  def index
+    @cocktails = Cocktail.all
+  end
+
+  def show
+    @dose = Dose.new
+  end
+
+  def new
+    @cocktail = Cocktail.new
+  end
+
+  def create
+    @cocktail = Cocktail.new(cocktail_params)
+    if @cocktail.save
+      redirect_to cocktails_path
+    else
+      render :new
+    end
+  end
+
+  private
+
+  def set_cocktail
+    @cocktail = Cocktail.find(params[:id])
+  end
+
+  def cocktail_params
+    params.require(:cocktail).permit(:name)
+  end
+
 end
