ee166c79da2d0115db0e3b6c31ab0fe30f428856
Daniel Tuppen <daniel.tuppen1@gmail.com>
2021-01-30 13:28:45 +0000

    added all ingredients for cocktails to db/seed file

--- db/seeds.rb
+++ db/seeds.rb
@@ -1,11 +1,59 @@
-# This file should contain all the record creation needed to seed the database with its default values.
-# The data can then be loaded with the bin/rails db:seed command (or created alongside the database with db:setup).
-#
-# Examples:
-#
-#   movies = Movie.create([{ name: 'Star Wars' }, { name: 'Lord of the Rings' }])
-#   Character.create(name: 'Luke', movie: movies.first)
+puts 'Cleaning database...'
+Ingredient.destroy_all
 
-Ingredient.create(name: "lemon")
-Ingredient.create(name: "ice")
+puts 'Creating ingredients...'
+
+# Pisco Sour
+Ingredient.create(name: "lime juice")
+Ingredient.create(name: "sugar")
+Ingredient.create(name: "pisco")
+Ingredient.create(name: "egg white")
+Ingredient.create(name: "angostura bitters")
 Ingredient.create(name: "mint leaves")
+
+# Caipirinha
+Ingredient.create(name: "cachaça")
+Ingredient.create(name: "crushed ice")
+
+# Margarita
+Ingredient.create(name: "salt")
+Ingredient.create(name: "tequila")
+Ingredient.create(name: "cointreau")
+
+# Manhatten
+Ingredient.create(name: "rye whiskey")
+Ingredient.create(name: "sweet vermouth")
+Ingredient.create(name: "ice")
+Ingredient.create(name: "maraschino cherry")
+Ingredient.create(name: "orange peel")
+
+# Negroni
+Ingredient.create(name: "gin")
+Ingredient.create(name: "campari")
+
+# Mohito
+Ingredient.create(name: "white rum")
+Ingredient.create(name: "soda")
+
+# Dark and Stormy
+Ingredient.create(name: "ginger beer")
+Ingredient.create(name: "dark rum")
+Ingredient.create(name: "lime wedge")
+
+# Espresso Martini
+Ingredient.create(name: "espresso")
+Ingredient.create(name: "kahlua")
+Ingredient.create(name: "vodka")
+Ingredient.create(name: "sugar syrup")
+Ingredient.create(name: "coffee beans")
+
+# Tom Collins
+Ingredient.create(name: "lemon juice")
+Ingredient.create(name: "grapefruit wedge")
+
+# Martini
+Ingredient.create(name: "dry vermouth")
+Ingredient.create(name: "olive")
+Ingredient.create(name: "lemon peel")
+
+puts 'Finished!'
